653439faa39e70d316da0fa2cbf80e89ca9ade47
kbooras <kbooras@ufl.edu>
2016-08-25 23:53:07 -0700

    Rename Firebase playlists

--- app/src/main/java/com/kirstiebooras/foodvids/firebase/Constants.java
+++ app/src/main/java/com/kirstiebooras/foodvids/firebase/Constants.java
@@ -3,8 +3,8 @@ package com.kirstiebooras.foodvids.firebase;
 public abstract class Constants {
 
     public static final String PLAYLIST_VIDEOS_REF = "playlistVideos";
-    public static final String BREAKFAST_PLAYLIST_VIDEOS_REF = "PLXBBwKNDaSnVLdmqoGmQto_GgtaSMjHGV";
-    public static final String SALAD_PLAYLIST_VIDEOS_REF = "PLXBBwKNDaSnUufhxBpoAWfYPmPPnwOGph";
-    public static final String ENTREE_PLAYLIST_VIDEOS_REF = "PLXBBwKNDaSnUz_cwcXZorX0GMIS7VOIdu";
-    public static final String DESSERTS_PLAYLIST_VIDEOS_REF = "PLXBBwKNDaSnV772FI0JUOwcWnroAGE2Al";
+    public static final String BREAKFAST_PLAYLIST_VIDEOS_REF = "breakfastList";
+    public static final String SALAD_PLAYLIST_VIDEOS_REF = "saladList";
+    public static final String ENTREE_PLAYLIST_VIDEOS_REF = "entreeList";
+    public static final String DESSERTS_PLAYLIST_VIDEOS_REF = "dessertList";
 }
